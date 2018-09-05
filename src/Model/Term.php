d0bcbe58ea5d9b09c0624a1ddcfad8287f06a443
Daen Rebel <daen@indexonline.nl>
2018-09-05 17:19:09 +0200

    Better slugify

--- src/Model/Term.php
+++ src/Model/Term.php
@@ -23,7 +23,7 @@ class Term extends Model
 
         //while creating/inserting item into db
         static::creating(function ($item) {
-            $slug = strtolower(str_replace(' ', '-', $item->name));
+            $slug = self::slugify($item->name);
             $item->slug = $slug;
         });
     }
@@ -57,5 +57,38 @@ class Term extends Model
     public function taxonomy()
     {
         return $this->hasOne(Taxonomy::class, 'term_id');
+    }
+
+    /**
+     * Slugify the text
+     *
+     * @param $text
+     * @return null|string|string[]
+     */
+    static public function slugify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // remove duplicate -
+        $text = preg_replace('~-+~', '-', $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
     }
 }
